f997a2d133589b46966fe83347a91e8fabb7131b
Serhii Pylypenko <s.pylypenko@mobilunity.com>
2025-09-22 19:28:57 +0200

    finish broadcasting

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -91,6 +91,10 @@
     <p class="m-0">Copyright &copy; {{date('Y')}} <a href="/" class="text-muted">OurApp</a>. All rights reserved.</p>
 </footer>
 
+@auth
+    <div dara-username="{{auth()->user()->username}}" data-avatar="{{auth()->user()->avatar}}" id="chat-wrapper" class="chat-wrapper shadow border-top border-left border-right"></div>
+@endauth
+
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
         integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
         crossorigin="anonymous"></script>
