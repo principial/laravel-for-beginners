9b8a40b8207e1a5e72b6c3c9cf1a599983f23a43
Serhii Pylypenko <s.pylypenko@mobilunity.com>
2025-09-27 13:03:26 +0200

    finish search

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,5 +1,5 @@
 <div x-data="{ isOpen: false}">
-    <button x-on:click="isOpen = true;"
+    <button x-on:click="isOpen = true; setTimeout(() => document.querySelector('#live-search-field').focus(), 100)"
             style="background: none;border: none;padding: 0;margin: 0; outline: none;cursor: pointer" href="#"
             class="text-white mr-2 header-search-icon" title="Search" data-toggle="tooltip"
             data-placement="bottom"><i class="fas fa-search"></i></button>
@@ -8,7 +8,7 @@
         <div class="search-overlay-top shadow-sm">
             <div class="container container--narrow">
                 <label for="live-search-field" class="search-overlay-icon"><i class="fas fa-search"></i></label>
-                <input wire:model.live.debounce.750ms="searchTerm" autocomplete="off" type="text" id="live-search-field"
+                <input x-on:keydown="document.querySelector('.circle-loader').classList.add('circle-loader--visible'); if (document.querySelector('#no-results')) {document.querySelector('#no-results').style.display = 'none'}" wire:model.live.debounce.750ms="searchTerm" autocomplete="off" type="text" id="live-search-field"
                        class="live-search-field" placeholder="What are you interested in?">
                 <span x-on:click="isOpen = false" class="close-live-search"><i class="fas fa-times-circle"></i></span>
             </div>
