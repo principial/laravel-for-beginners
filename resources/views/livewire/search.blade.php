b46c73a0f32bd58bdb0d589b1262a3255958b8bc
s.pylypenko <s.pylypenko@mobilunity.com>
2025-09-27 12:21:46 +0200

    search

--- resources/views/livewire/search.blade.php
+++ resources/views/livewire/search.blade.php
@@ -1,5 +1,6 @@
 <div x-data="{ isOpen: false}">
-    <button x-on:click="isOpen = true;" style="background: none;border: none;padding: 0;margin: 0; outline: none;cursor: pointer" href="#"
+    <button x-on:click="isOpen = true;"
+            style="background: none;border: none;padding: 0;margin: 0; outline: none;cursor: pointer" href="#"
             class="text-white mr-2 header-search-icon" title="Search" data-toggle="tooltip"
             data-placement="bottom"><i class="fas fa-search"></i></button>
 
@@ -7,16 +8,35 @@
         <div class="search-overlay-top shadow-sm">
             <div class="container container--narrow">
                 <label for="live-search-field" class="search-overlay-icon"><i class="fas fa-search"></i></label>
-                <input autocomplete="off" type="text" id="live-search-field" class="live-search-field" placeholder="What are you interested in?">
-                <span class="close-live-search"><i class="fas fa-times-circle"></i></span>
+                <input wire:model.live.debounce.750ms="searchTerm" autocomplete="off" type="text" id="live-search-field"
+                       class="live-search-field" placeholder="What are you interested in?">
+                <span x-on:click="isOpen = false" class="close-live-search"><i class="fas fa-times-circle"></i></span>
             </div>
         </div>
 
         <div class="search-overlay-bottom">
             <div class="container container--narrow py-3">
                 <div class="circle-loader"></div>
-                <div class="live-search-results"></div>
+                <div class="live-search-results live-search-results--visible">
+                    <div class="list-group shadow-sm">
+                        @if (count($results) == 0 && $searchTerm !== "")
+                            <p id="no-results" class="alert alert-danger">Nothing found</p>
+                        @endif
+                        @if (count($results) > 0)
+                            <div class="list-group-item active"><strong>Search Results</strong>
+                                ({{count($results)}} {{count($results) > 1 ? 'results' : 'result'}})
+                            </div>
+                            @foreach($results as $post)
+                                <a href="/post/{{$post->id}}" class="list-group-item list-group-item-action">
+                                    <img class="avatar-tiny" src="{{$post->user->avatar}}">
+                                    <strong>{{$post->title}}</strong>
+                                    <span
+                                        class="text-muted small">by {{$post->user->username}} on {{$post->created_at->format('d-m-Y')}}</span>
+                                </a>
+                            @endforeach
+                        @endif
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
